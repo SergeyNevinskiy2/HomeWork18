980bc02264d23aeef17ab79ef431a1398cd206b1
SergeyNevinskiy <s.nevinskiy1987@ukr.net>
2022-07-24 19:03:34 +0300

    ok

--- script/script.js
+++ script/script.js
@@ -1,30 +1,23 @@
 'use strict'
-function padString(str, num, char, symbRightOrLeft = true) {
-  if (typeof str !== 'string' || str.trim() === '') {
-      return "Invalid string";
-  }
-  if (isNaN(num) || num <= 0) {
-  return "Error - not a number";
- }
- if (char === null) {
-  return "Invalid symbol";
- }
- if (typeof symbRightOrLeft !== 'boolean') {
-  return "Error - not boolean";
- }
- let change = num - str.length;
- if (num > str.length) {
-  for (let i = 0; i < change; i++) {
-    symbRightOrLeft ? (str += char) : (str = char + str);
-  }  
-  return str;
-} 
-if ((num < str.length) || (num === str.length))   {
-  str = str.substring(0, num);
-  return str;
-} 
-} 
 
-console.log(padString("hello", 8, "*"));
-console.log(padString("hello", 6, "*", false));
-console.log(padString("hello", 2));
+
+const padString = (string, srtLenght, symbol, right = true) => {
+  if (typeof string !== 'string') throw new Error('is not in type string');
+  if (typeof srtLenght !== 'number') throw new Error('is no in number');
+  if (string.length === srtLenght) return string;
+  if (string.length > srtLenght) return string.substring(0, srtLenght);
+
+  if (typeof symbol !== 'string') throw new Error('symbol is not string');
+  if (symbol.length > 1) throw new Error (' symbol length long');
+  if (typeof right !== 'boolean') throw new Error('right is not boolean');
+
+  const symbols = symbol.repeat(srtLenght - string.length);
+
+  return right ? string + symbols : symbols + string;
+
+}
+
+
+console.log(padString('hello', 2))
+console.log(padString('hello',8, '*'))
+
